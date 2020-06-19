3bf96fa842b87b7aa701319278e68a46c29f241b
Bryan Oyloe <boyloe@gmail.com>
2020-06-19 04:35:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,10 +22,9 @@ end
 
 def return_string_array(array)
   count = 0
-  new_arr = []
   while count < array.length do
-    new_arr.join(array[count].to_s)
+    array[count] = array[count].to_s
     count += 1
   end
-  new_arr
+  array
 end 
